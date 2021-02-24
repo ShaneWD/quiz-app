dc5a7964f5b2ec4abb75ac98026cf2cd6e845a4b
Shane <>
2021-02-24 12:25:16 -0500

    added User to admin pannel, removed password

--- QUIZ_APP/users/admin.py
+++ QUIZ_APP/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+class AdminConfig(admin.ModelAdmin):
+    exclude = ('password',)
+
+admin.site.register(User, AdminConfig)
