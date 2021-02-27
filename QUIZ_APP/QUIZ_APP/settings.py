120dbdf7b3696a919ea17232781f190737829f99
Shane <>
2021-02-26 22:52:35 -0500

    now uses PostgreSQL

--- QUIZ_APP/QUIZ_APP/settings.py
+++ QUIZ_APP/QUIZ_APP/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'QUIZ_APP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
