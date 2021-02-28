43e8341fb8006cc4ce6d3f8948fb29ac77105c5d
Shane <>
2021-02-28 13:36:49 -0500

    admin pannel shows more info

--- QUIZ_APP/users/admin.py
+++ QUIZ_APP/users/admin.py
@@ -4,5 +4,6 @@ from .models import User
 
 class AdminConfig(admin.ModelAdmin):
     exclude = ('password',)
+    list_display = ('username', 'date_joined', 'is_staff', 'is_superuser')
 
 admin.site.register(User, AdminConfig)
