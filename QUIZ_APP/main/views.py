925895281616ced7abf47b81d58720d9d45e8987
Shane <>
2021-02-28 13:46:06 -0500

    now utilizes home.html template

--- QUIZ_APP/main/views.py
+++ QUIZ_APP/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1>Hello</h1>")
+    context = {
+
+    }
+    return render (request, "main/home.html", context)
