9ee84cc6a7d0af2ef9ba3d63576c07db16ab0b9e
Shane <>
2021-03-03 14:10:56 -0500

    set up models fields for quiz

--- QUIZ_APP/main/models.py
+++ QUIZ_APP/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=100)
+    data = models.JSONField()
+
+    created = models.DateTimeField( auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
